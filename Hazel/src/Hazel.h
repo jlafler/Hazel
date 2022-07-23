44a4f4929e29ab71394369f8c9e955885e478ec4
Jesse <jesselafler11@gmail.com>
2022-07-22 20:15:53 -0500

    Setup Basic App and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+// for use by Hazel Applications
+
+#include "Hazel/Application.h"
+#include <stdio.h>;
+
+//---------ENTRY POINT--------------------------------------
+#include "Hazel/EntryPoint.h"
---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else
+		#define HAZEL_API __declspec(dllimport)
+	#endif // HZ_BUILD_DLL
+#else
+#error Hazel Only Supports Windows!
+#endif
+
